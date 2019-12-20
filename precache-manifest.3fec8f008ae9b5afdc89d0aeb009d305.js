ee01216b86e943fa73253834e0f2ebcfdf164ccc
Andrija <AndrijaS37N@users.noreply.github.com>
2019-12-20 19:57:45 +0100

    Site refreshed.

--- precache-manifest.3fec8f008ae9b5afdc89d0aeb009d305.js
+++ precache-manifest.3fec8f008ae9b5afdc89d0aeb009d305.js
@@ -1,43 +1,55 @@
 self.__precacheManifest = [
   {
-    "revision": "b2972f67fd4476cdf568",
-    "url": "/css/about.a0076315.css"
+    "revision": "d169731bea1a46f2b238",
+    "url": "/css/about.a6442961.css"
   },
   {
-    "revision": "b2972f67fd4476cdf568",
-    "url": "/js/about.b6512e35.js"
+    "revision": "d169731bea1a46f2b238",
+    "url": "/js/about.31921d79.js"
   },
   {
-    "revision": "9c756deb6d23bcca2050",
-    "url": "/css/app.c43828b4.css"
+    "revision": "0dcfd45c18e70aba6907",
+    "url": "/css/app.b3d4bb08.css"
   },
   {
-    "revision": "9c756deb6d23bcca2050",
-    "url": "/js/app.e9abbcd6.js"
+    "revision": "0dcfd45c18e70aba6907",
+    "url": "/js/app.5b788de3.js"
   },
   {
     "revision": "055fdbd059c53329d2c6",
     "url": "/js/chunk-vendors.46128ea7.js"
   },
   {
-    "revision": "f0b3e4c4ac090ace0cf2f5b475951597",
-    "url": "/img/call-me.f0b3e4c4.png"
+    "revision": "75a92bd5223400eac535cf15d6e91181",
+    "url": "/img/earth_pic.75a92bd5.png"
   },
   {
     "revision": "3c9fc558c585289ff41830144194d7cf",
-    "url": "/img/me.3c9fc558.jpg"
+    "url": "/img/portrait_pic.3c9fc558.jpg"
   },
   {
-    "revision": "75a92bd5223400eac535cf15d6e91181",
-    "url": "/img/earth.75a92bd5.png"
+    "revision": "f0b3e4c4ac090ace0cf2f5b475951597",
+    "url": "/img/call_gesture_pic.f0b3e4c4.png"
   },
   {
-    "revision": "5daef53ad9cbae3c6fa6f7a25eef5d6c",
+    "revision": "9a4cb0535c17dad649ebcd886e53bc86",
     "url": "/index.html"
   },
   {
     "revision": "936d6e411cabd71f0e627011c3f18fe2",
     "url": "/img/icons.bak/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons.bak/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons.bak/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons.bak/apple-touch-icon.png"
   },
   {
     "revision": "1a034e64d80905128113e5272a5ab95e",
@@ -48,18 +60,6 @@ self.__precacheManifest = [
     "url": "/img/icons.bak/apple-touch-icon-60x60.png"
   },
   {
-    "revision": "af28d69d59284dd202aa55e57227b11b",
-    "url": "/img/icons.bak/apple-touch-icon-76x76.png"
-  },
-  {
-    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
-    "url": "/img/icons.bak/apple-touch-icon-180x180.png"
-  },
-  {
-    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
-    "url": "/img/icons.bak/apple-touch-icon.png"
-  },
-  {
     "revision": "4bb1a55479d61843b89a2fdafa7849b3",
     "url": "/img/icons.bak/favicon-16x16.png"
   },
@@ -68,20 +68,20 @@ self.__precacheManifest = [
     "url": "/img/icons.bak/favicon-32x32.png"
   },
   {
-    "revision": "b89032a4a5a1879f30ba05a13947f26f",
-    "url": "/img/icons.bak/msapplication-icon-144x144.png"
-  },
-  {
     "revision": "058a3335d15a3eb84e7ae3707ba09620",
     "url": "/img/icons.bak/mstile-150x150.png"
   },
   {
-    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons.bak/msapplication-icon-144x144.png"
   },
   {
     "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
     "url": "/img/icons.bak/android-chrome-192x192.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
   },
   {
     "revision": "f22d501a35a87d9f21701cb031f6ea17",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2400db2ef1dc06f9043c8260aa90879.js"
+  "/precache-manifest.3fec8f008ae9b5afdc89d0aeb009d305.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "my-portfolio"});
